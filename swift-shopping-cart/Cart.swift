7629ac345eac7bf8fab6d28760b17a3301600644
henrykchan <chan.henryk@gmail.com>
2016-09-30 18:13:17 -0400

    shopping cart 💯

--- swift-shopping-cart/Cart.swift
+++ swift-shopping-cart/Cart.swift
@@ -8,5 +8,104 @@
 
 class Cart {
     
+    var items = [Item]()
+    
+    
+    func totalPriceInCents () -> Int {
+        
+        var sum = 0
+        for price in items {
+            
+            sum = price.priceInCents + sum
+        }
+        
+        
+        return sum
+    }
+    
+    
+    
+    
+    func add (item:Item) {
+        
+        items.append(item)
+        
+    }
+    
+    
+    
+    func remove (item:Item) {
+
+    
+            if let item = items.index(of: item) {
+    
+                items.remove(at: item)
+            }
+    }
+
+    
+    
+    func items (withName name:String) -> [Item] {
+        
+        var newItemArray = [Item]()
+        
+        for item in items {
+            
+            if item.name == name {
+                
+                newItemArray.append(item)
+            }
+        }
+        
+        
+        return newItemArray
+        
+    }
+    
+    
+    
+    
+    func items(withMinPrice price:Int) -> [Item] {
+        
+        
+        var newItemArray = [Item]()
+        
+        for item in items {
+            
+            if item.priceInCents >= price {
+                
+                newItemArray.append(item)
+            }
+        }
+        
+        return newItemArray
+    }
+    
+    
+    
+    
+    func items(withMaxPrice price:Int) -> [Item] {
+        
+        var newItemArray = [Item]()
+        
+        for item in items {
+            
+            if item.priceInCents <= price {
+                
+                newItemArray.append(item)
+            }
+        }
+        
+        return newItemArray
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
